da3b29c65d3a657e420ca3df1198bbe330cf6ba9
renestauber <78612447+homepageschmiede@users.noreply.github.com>
2021-02-06 13:44:42 +0100

    rene

--- agenda-app/start-here/src/App.vue
+++ agenda-app/start-here/src/App.vue
@@ -33,7 +33,7 @@ export default {
       events: [
         {
           time: "10:00",
-          title: "Breakfast with Simon1234 ",
+          title: "Breakfast with Simon12345 ",
           location: "Lounge Caffe",
           description: "Discuss Q3 targets"
         },
