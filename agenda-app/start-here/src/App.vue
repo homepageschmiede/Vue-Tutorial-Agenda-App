f320e276e3e14007fa46e920e81aa51717ce59d0
renestauber <78612447+homepageschmiede@users.noreply.github.com>
2021-02-06 13:56:48 +0100

    Rene 1356

--- agenda-app/start-here/src/App.vue
+++ agenda-app/start-here/src/App.vue
@@ -2,23 +2,54 @@
   <mdb-container>
     <mdb-row>
       <mdb-col col="9">
-<Event
-  v-for="(event, index) in events"
-  :index="index"
-  :time="event.time"
-  :title="event.title"
-  :location="event.location"
-  :description="event.description"
-  :key="index"
-/>
+        <h2 class="text-uppercase my-3">Today:</h2>
+        <Event
+          v-for="(event, index) in events"
+          :index="index"
+          :time="event.time"
+          :title="event.title"
+          :location="event.location"
+          :description="event.description"
+          :key="index"
+        />
+        <mdb-row>
+          <mdb-col xl="3" md="6" class="mx-auto text-center">
+            <mdb-btn color="info">Add Event</mdb-btn>
+          </mdb-col>
+        </mdb-row>
       </mdb-col>
-      <mdb-col col="3"></mdb-col>
+      <mdb-col col="3">
+        <h3 class="text-uppercase my-3">Schedule</h3>
+        <h6 class="my-3">
+          It's going to be busy that today. You have
+          <b>{{events.length}} events</b> today.
+        </h6>
+        <h1 class="my-3">
+          <mdb-row>
+            <mdb-col col="3" class="text-center">
+              <mdb-icon far icon="sun"/>
+            </mdb-col>
+            <mdb-col col="9">Sunny</mdb-col>
+          </mdb-row>
+          <mdb-row>
+            <mdb-col col="3" class="text-center">
+              <mdb-icon icon="thermometer-three-quarters"/>
+            </mdb-col>
+            <mdb-col col="9">23°C</mdb-col>
+          </mdb-row>
+        </h1>
+        <p>
+          Don't forget your sunglasses. Today will dry and sunny, becoming
+          warm in the afternoon with temperatures of between 20 and 25
+          degrees.
+        </p>
+      </mdb-col>
     </mdb-row>
   </mdb-container>
 </template>
 
 <script>
-import { mdbContainer, mdbRow, mdbCol } from "mdbvue";
+import { mdbContainer, mdbRow, mdbCol, mdbIcon, mdbBtn } from "mdbvue";
 import Event from "@/components/Event";
 export default {
   name: "App",
@@ -26,7 +57,9 @@ export default {
     mdbContainer,
     mdbRow,
     mdbCol,
-    Event
+    Event,
+    mdbIcon,
+    mdbBtn
   },
     data() {
     return {
@@ -57,10 +90,3 @@ export default {
   }
 };
 </script>
-
-<style>
-div[class^="col"],
-div[class*=" col"] {
-  border: 1px dotted black;
-}
-</style>
