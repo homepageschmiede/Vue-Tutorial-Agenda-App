2c34379ff0c3184b0ffd8ce4920b5a746ef4fe92
renestauber <78612447+homepageschmiede@users.noreply.github.com>
2021-02-06 13:33:11 +0100

    test

--- agenda-app/start-here/src/App.vue
+++ agenda-app/start-here/src/App.vue
@@ -1,23 +1,66 @@
 <template>
-  <div class="app">
-    <h2>I am App (main component)</h2>
-    <HelloWorld/>
-  </div>
+  <mdb-container>
+    <mdb-row>
+      <mdb-col col="9">
+<Event
+  v-for="(event, index) in events"
+  :index="index"
+  :time="event.time"
+  :title="event.title"
+  :location="event.location"
+  :description="event.description"
+  :key="index"
+/>
+      </mdb-col>
+      <mdb-col col="3"></mdb-col>
+    </mdb-row>
+  </mdb-container>
 </template>
 
 <script>
+import { mdbContainer, mdbRow, mdbCol } from "mdbvue";
+import Event from "@/components/Event";
 export default {
-  name: "App"
+  name: "App",
+  components: {
+    mdbContainer,
+    mdbRow,
+    mdbCol,
+    Event
+  },
+    data() {
+    return {
+      events: [
+        {
+          time: "10:00",
+          title: "Breakfast with Simon",
+          location: "Lounge Caffe",
+          description: "Discuss Q3 targets"
+        },
+        {
+          time: "10:30",
+          title: "Daily Standup Meeting (recurring)",
+          location: "Warsaw Spire Office"
+        },
+        {
+          time: "11:00",
+          title: "Call with HRs"
+        },
+        {
+          time: "12:00",
+          title: "Lunch with Timmoty",
+          location: "Canteen",
+          description: "Project evalutation"
+        }
+      ]
+    };
+  }
 };
 </script>
 
 <style>
-.app {
-  margin: 0 auto;
-  font-family: sans-serif;
-  background-color: #ccf7e2;
-  padding: 10px;
-  border-radius: 5px;
-  max-width: 500px;
+div[class^="col"],
+div[class*=" col"] {
+  border: 1px dotted black;
 }
 </style>
